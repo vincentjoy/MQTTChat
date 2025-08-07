0c615eba1ce501f1984bc3a6af82dab92b6b1cde
Vincent Joy <vincentjoyhere@gmail.com>
2025-08-07 20:05:03 +0530

    Initial Commit

---
+++ MQTTChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MQTTChat
+//
+//  Created by Vincent Joy on 07/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MQTTChat/MQTTChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MQTTChatApp.swift
+//  MQTTChat
+//
+//  Created by Vincent Joy on 07/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MQTTChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
